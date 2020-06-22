f8414d6dd50b664d9b524e53ad3375e3384ebf9a
jfbm74 <jfbm74@gmail.com>
2020-06-22 12:58:51 -0500

    Adding project

---
+++ 0x05-processes_and_signals/67-kill_me_now_made_easy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# script that kills 4-to_infinity_and_beyond process without kill cmd
+
+pkill -f "7-highlander.sh"
---
+++ 0x05-processes_and_signals/7-highlander.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Displays infinity and beyond indefinitely
+
+trap "echo 'I am invincible!!!'" 15
+while :
+do
+      echo "To infinity and beyond"
+      sleep 2
+done
