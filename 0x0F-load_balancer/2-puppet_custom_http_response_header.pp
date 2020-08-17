22c7d3e4edc014965b236ad066ec446d4bb98629
jfbm74 <jfbm74@gmail.com>
2020-08-17 11:13:08 -0500

    Adding puppet file configutation

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,24 @@
+# Puppet file
+
+exec {'update':
+  command  => 'sudo apt-get update',
+  provider => shell,
+}
+
+package {'nginx':
+  ensure  => installed,
+  require => Exec['update'],
+}
+
+file_line {'response_header':
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => "add_header X-Served-By ${hostname};",
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => File_line['response_header'],
+}
