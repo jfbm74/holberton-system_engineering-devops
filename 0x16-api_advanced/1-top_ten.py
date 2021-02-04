44b28afb74d4377e47d1830c4c621aa1b70e019f
jfbm74 <jfbm74@gmail.com>
2021-02-04 14:57:07 -0500

    Adding Tasks 0-1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,11 +11,12 @@ def top_ten(subreddit):
     listed for a given subreddit.]
 
     Args:
-        subreddit ([string]): [topic of subredit]
+        subreddit ([string]): [subs.py:21:3]
     """
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    data = requests.get(url, headers={"User-Agent": "yuyo2211"}, allow_redirects=False)
-    
+    data = requests.get(url, headers={"User-Agent": "yuyo2211"},
+                        allow_redirects=False)
+
     if data.status_code == 200:
         for subredit in data.json()['data']['children']:
             print(subredit['data']['title'])
