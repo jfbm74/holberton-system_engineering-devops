098742e5d0b6c48e0face71cf52ae8f541fed936
jfbm74 <jfbm74@gmail.com>
2021-02-04 09:04:10 -0500

    function that queries the Reddit API

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module for subreddit subscribers
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """[function that queries the Reddit API
+    and returns the number of subscribers for a given subreddit.]
+
+    Args:
+        subreddit ([string]): [topic of subredit]
+
+    Returns:
+        [int]: [Number of subscribers for a given topic]
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    data = requests.get(url, headers={"User-Agent": "yuyo2211"},
+                              allow_redirects=False)
+    if data.status_code == 200:
+        return data.json()['data']['subscribers']
+    else:
+        return 0
